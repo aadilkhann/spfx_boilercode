391b5b3e85d0e2dcd4791840912a8842e3d64590
Adil Khan <khan01970@gmail.com>
2024-02-05 10:28:47 +0530

    Added Functionality -Update -Delete

--- src/webparts/practice/components/TestComponent.tsx
+++ src/webparts/practice/components/TestComponent.tsx
@@ -5,7 +5,7 @@ import { sp } from "@pnp/sp/presets/all";
 function TestComponent() {
   const getDataFromList = async () => {
     let res = await sp.web.lists.getByTitle("TestList").items.get();
-    console.log("ResponseData", res);
+    console.log("ResponseData of Get Element", res);
   };
 
   const insertDataIntoList = async () => {
@@ -13,12 +13,27 @@ function TestComponent() {
       Title: "Mr.",
       Name: "Babu",
     });
-    console.log("ResponseData", res);
+    console.log("ResponseData of Push Element", res);
   };
 
+  const list = sp.web.lists.getByTitle("TestList");
+  const updateDataList = async () => {
+    let res = await list.items.getById(3).update({
+      Title: "New Title2",
+      Name: "New Name2",
+    });
+    console.log("ResponseData of Update Element", res);
+  };
+
+  const deleteDataList = async () => {
+    let res = await list.items.getById(3).delete();
+    console.log("ResponseData of Delete Element", res);
+  };
   React.useEffect(() => {
     void getDataFromList();
     void insertDataIntoList();
+    void updateDataList();
+    void deleteDataList();
   }, []);
 
   return <div>TestComponent</div>;
